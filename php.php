1434bf2fe57f1d6da8861e5cf88cf07a98b4fc59
istiaque ahmed <istiaque.ahmed31122003@gmail.com>
2026-08-01 22:16:15 +0600

    done

---
+++ php.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <style>
+    .chat{
+      position:fixed;
+      right: 20px;
+      bottom: 20px;
+    }
+  </style>
+  <title>Add Remove Element</title>
+</head>
+<body>
+
+<p>this is my webside</p>
+<?php 
+$name = "istiaque";
+$age = 10;
+echo "my name is ".$name;
+echo "my age is ".$age;
+?>
+
+
+<script>
+
+</script>
+
+</body>
+</html>
